4a19e1f1ff6a9ba47a17889b3147fdd9fbd0d502
amateur_dev <me@dipeshsukhani.dev>
2021-09-14 17:46:51 +0800

    Refactor
    Refactoring ERC721Permit in line with ERC20Permit

--- contracts/base/ERC721Permit.sol
+++ contracts/base/ERC721Permit.sol
@@ -8,40 +8,23 @@ import {IERC721Permit} from '../interfaces/IERC721Permit.sol';
 import {ERC721} from './ERC721.sol';
 import '../interfaces/IERC1271.sol';
 import '../interfaces/IERC721Permit.sol';
-import './BlockTimestamp.sol';
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
-abstract contract ERC721Permit is BlockTimestamp, ERC721, IERC721Permit, EIP712 {
+abstract contract ERC721Permit is ERC721, IERC721Permit, EIP712 {
     using Counters for Counters.Counter;
     
     mapping(uint256 => Counters.Counter) private _nonces;
 
-    /// @notice Computes the nameHash and versionHash
+    bytes32 public immutable override _PERMIT_TYPEHASH =
+        keccak256("Permit(address spender,uint256 tokenId,uint256 nonce,uint256 deadline)");
+
+
     constructor(
         string memory name_
     ) EIP712(name_, "1"){}
 
-    /// @inheritdoc IERC721Permit
-    function DOMAIN_SEPARATOR() public view override returns (bytes32) {
-        return
-            keccak256(
-                abi.encode(
-                    // keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)')
-                    0x8b73c3c69bb8fe3d512ecc4cf759cc79239f7b179b0ffacaa9a75d522b39400f,
-                    nameHash,
-                    versionHash,
-                    ChainId.get(),
-                    address(this)
-                )
-            );
-    }
-
-    /// @inheritdoc IERC721Permit
-    /// @dev Value is equal to keccak256("Permit(address spender,uint256 tokenId,uint256 nonce,uint256 deadline)");
-    /// @dev Since tokenId is there, the specific field of owner is not required;
-    bytes32 public constant override PERMIT_TYPEHASH =
-        0x49ecf333e5b8c95c40fdafc95c1ad136e8914a8fb55e9dc8bb01eaa83a2df9ad;
-
+    
     /// @inheritdoc IERC721Permit
     function permit(
         address spender,
@@ -51,19 +34,19 @@ abstract contract ERC721Permit is BlockTimestamp, ERC721, IERC721Permit, EIP712 
         bytes32 r,
         bytes32 s
     ) external payable override {
-        require(_blockTimestamp() <= deadline, 'Permit expired');
+        require(block.timestamp <= deadline, 'Permit expired');
 
-        bytes32 digest =
-            keccak256(
-                abi.encodePacked(
-                    '\x19\x01',
-                    DOMAIN_SEPARATOR(),
-                    keccak256(abi.encode(PERMIT_TYPEHASH, spender, tokenId, _useNonce(tokenId), deadline))
-                )
-            );
+        bytes32 structHash = keccak256(abi.encode(_PERMIT_TYPEHASH, spender, tokenId, _useNonce(tokenId), deadline));
+
+        bytes32 hash = _hashTypedDataV4(structHash);
+
+        address signer = ECDSA.recover(hash, v, r, s);
+        require(signer == owner, "ERC20Permit: invalid signature");
+        
         address owner = ownerOf[tokenId];
         require(spender != owner, 'ERC721Permit: approval to current owner');
 
+        //TODO: to check on isValidSignature
         if (Address.isContract(owner)) {
             require(IERC1271(owner).isValidSignature(digest, abi.encodePacked(r, s, v)) == 0x1626ba7e, 'Unauthorized');
         } else {
@@ -77,6 +60,10 @@ abstract contract ERC721Permit is BlockTimestamp, ERC721, IERC721Permit, EIP712 
 
     function nonces(uint256 tokenId) public view virtual returns (uint256) {
         return _nonces[tokenId].current();
+    }
+
+    function DOMAIN_SEPARATOR() external view override returns (bytes32) {
+        return _domainSeparatorV4();
     }
 
     function _useNonce(uint256 tokenId) internal virtual returns (uint256 current) {
