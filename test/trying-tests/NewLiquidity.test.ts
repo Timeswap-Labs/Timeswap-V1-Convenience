695a19ed23afee1d75bf6e6f92d750ba413a48f7
Emmanuel Antony <emmanuelantony2000@gmail.com>
2021-10-06 16:35:51 +0000

    New Liquidity update
    - Updated to uint
    - Cleanup left
    - Checks are separate and not using throws
    - Have to add extra tests for debt and all in new liq

--- test/trying-tests/NewLiquidity.test.ts
+++ test/trying-tests/NewLiquidity.test.ts
@@ -1,17 +1,29 @@
 import { ethers, waffle } from 'hardhat'
-import { now } from '../shared/Helper'
+import { mulDivUp, now, shiftUp } from '../shared/Helper'
 import { expect } from '../shared/Expect'
 import { newLiquidityTests } from '../test-cases'
 import { newLiquidityFixture, constructorFixture, Fixture } from '../shared/Fixtures'
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import * as fc from 'fast-check'
 import { NewLiquidityParams } from '../test-cases/types'
-import { ERC20__factory } from '../../typechain'
+import { ERC20__factory, CollateralizedDebt__factory } from '../../typechain'
+import { Uint112, Uint256 } from '@timeswap-labs/timeswap-v1-sdk-core'
+import { Uint } from '@timeswap-labs/timeswap-v1-sdk-core/dist/uint/uint'
 
 const { loadFixture } = waffle
 
 let maturity = 0n
 let signers: SignerWithAddress[] = []
+
+function check(num: Uint, bit: bigint) {
+  return num.toBigInt() < 1n << bit
+}
+
+interface NLParams {
+  assetIn: Uint112
+  debtIn: Uint112
+  collateralIn: Uint112
+}
 
 const MAXUINT112: bigint = 2n ** 112n
 
@@ -25,29 +37,41 @@ describe('New Liquidity', () => {
     return constructor
   }
 
-  const checkYAndZIncrease = (assetIn: bigint, debtIn: bigint, collateralIn: bigint, currentTime: bigint) => {
+  const verifyYAndZIncrease = ({ assetIn, debtIn, collateralIn }: NLParams, currentTime: bigint) => {
     // const ct = BigInt(Math.floor(Date.now() / 1000))
 
-    const yIncrease = ((debtIn - assetIn) << 32n) / (maturity - currentTime)
-    const denominator = (maturity - currentTime) * yIncrease + (assetIn << 33n)
-    const zIncrease = ((collateralIn * assetIn) << 32n) / denominator
+    const yIncrease = new Uint256(debtIn)
+      .sub(assetIn)
+      .shiftLeft(32)
+      .div(maturity - currentTime)
 
-    return yIncrease > 0n && zIncrease > 0n && yIncrease < MAXUINT112 && zIncrease < MAXUINT112
+    const denominator = new Uint256(maturity - currentTime).mul(yIncrease).add(new Uint256(assetIn).shiftLeft(33))
+    const zIncrease = new Uint256(collateralIn).mul(assetIn).shiftLeft(32).div(denominator)
+
+    return { yIncrease, zIncrease }
   }
-  function filterSuccessNewLiquidity(newLiquidityParams: NewLiquidityParams, currentTime: bigint) {
-    if (newLiquidityParams.assetIn <= 0) {
-      return false
-    }
+
+  function filterSuccessNewLiquidity(newLiquidityParams: NLParams, currentTime: bigint) {
     if (
-      !checkYAndZIncrease(
-        newLiquidityParams.assetIn,
-        newLiquidityParams.debtIn,
-        newLiquidityParams.collateralIn,
-        currentTime
-      )
+      newLiquidityParams.assetIn.toBigInt() >= newLiquidityParams.debtIn.toBigInt() ||
+      newLiquidityParams.assetIn.toBigInt() === 0n ||
+      newLiquidityParams.debtIn.toBigInt() === 0n ||
+      newLiquidityParams.collateralIn.toBigInt() === 0n
     ) {
       return false
     }
+
+    const { yIncrease, zIncrease } = verifyYAndZIncrease(newLiquidityParams, currentTime)
+
+    if (
+      yIncrease.toBigInt() === 0n ||
+      zIncrease.toBigInt() === 0n ||
+      !check(yIncrease, 112n) ||
+      !check(zIncrease, 112n)
+    ) {
+      return false
+    }
+
     return true
   }
 
@@ -59,22 +83,57 @@ describe('New Liquidity', () => {
       return ((assetIn << 56n) * 0x10000000000n) / ((maturity - newCurrentTime) * 50n + 0x10000000000n)
     }
 
+    const debtCollateralCalculate = ({ assetIn, debtIn, collateralIn }: NLParams, currentTime: bigint) => {
+      const yIncrease = new Uint112(
+        new Uint256(debtIn)
+          .sub(assetIn)
+          .shiftLeft(32)
+          .div(maturity - currentTime)
+      )
+      const denominator = new Uint256(maturity - currentTime).mul(yIncrease).add(new Uint256(assetIn).shiftLeft(33))
+      const zIncrease = new Uint112(new Uint256(collateralIn).mul(assetIn).shiftLeft(32).div(denominator))
+
+      const debt = shiftUp(new Uint256(yIncrease).mul(maturity - currentTime).toBigInt(), 32n) + assetIn.toBigInt()
+      const collateral = mulDivUp(
+        new Uint256(yIncrease)
+          .mul(maturity - currentTime)
+          .add(new Uint256(assetIn).shiftLeft(33))
+          .toBigInt(),
+        zIncrease.toBigInt(),
+        new Uint256(assetIn).shiftLeft(32).toBigInt()
+      )
+
+      return { debt, collateral }
+    }
+
     await fc.assert(
       fc.asyncProperty(
         fc
           .record({ assetIn: fc.bigUintN(112), debtIn: fc.bigUintN(112), collateralIn: fc.bigUintN(112) })
+          .map(({ assetIn, debtIn, collateralIn }) => {
+            return {
+              assetIn: new Uint112(assetIn),
+              debtIn: new Uint112(debtIn),
+              collateralIn: new Uint112(collateralIn),
+            }
+          })
           .filter((x) => filterSuccessNewLiquidity(x, currentTime)),
         async (data) => {
           const success = async () => {
             const constructor = await loadFixture(fixture)
-            const newLiquidity = await newLiquidityFixture(constructor, signers[0], data)
+            const newLiquidity = await newLiquidityFixture(constructor, signers[0], {
+              assetIn: data.assetIn.toBigInt(),
+              debtIn: data.debtIn.toBigInt(),
+              collateralIn: data.collateralIn.toBigInt(),
+            })
             return newLiquidity
           }
 
           const result = await loadFixture(success)
           const newCurrentTime = await now()
-          const liquidityBalance = liquidityCalculate(data.assetIn, newCurrentTime)
-          console.log(data)
+          const liquidityBalance = liquidityCalculate(data.assetIn.toBigInt(), newCurrentTime)
+          const { debt, collateral } = debtCollateralCalculate(data, newCurrentTime)
+          // console.log(data)
           // console.log(liquidityBalance)
           // console.log((maturity - currentTime))
 
@@ -84,14 +143,28 @@ describe('New Liquidity', () => {
           // expect((await result.collateralToken.balanceOf(signers[0].address)).toBigInt()).equalBigInt(
           //   (1n << 150n) - data.collateralIn
           // )
-          const liquidityToken = ERC20__factory.connect(
-            (await result.convenience.getNatives(result.assetToken.address, result.collateralToken.address, maturity))
-              .liquidity,
-            ethers.provider
+          const natives = await result.convenience.getNatives(
+            result.assetToken.address,
+            result.collateralToken.address,
+            maturity
           )
+
+          const liquidityToken = ERC20__factory.connect(natives.liquidity, ethers.provider)
           const liquidityBalanceContract = (await liquidityToken.balanceOf(signers[0].address)).toBigInt()
           // console.log(liquidityBalanceContract)
           expect(liquidityBalanceContract).equalBigInt(liquidityBalance)
+
+          const collateralizedDebtContract = CollateralizedDebt__factory.connect(
+            natives.collateralizedDebt,
+            ethers.provider
+          )
+          const collateralizedDebtToken = await collateralizedDebtContract.dueOf(0)
+
+          const collateralBalanceContract = collateralizedDebtToken.collateral.toBigInt()
+          const debtBalanceContract = collateralizedDebtToken.debt.toBigInt()
+
+          expect(collateralBalanceContract).equalBigInt(collateral)
+          expect(debtBalanceContract).equalBigInt(debt)
         }
       )
     )
