ccb74b65337cb7ca4c7c5c71af2d88a70dcb883a
Ricsson Ngo <mathepreneur.eth@gmail.com>
2022-03-10 15:41:44 +0400

    use shift right 1 instead of divide 2

--- contracts/libraries/SquareRoot.sol
+++ contracts/libraries/SquareRoot.sol
@@ -4,11 +4,11 @@ pragma solidity =0.8.4;
 library SquareRoot {
     // babylonian method (https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method)
     function sqrt(uint256 x) internal pure returns (uint256 y) {
-        uint256 z = (x + 1) / 2;
+        uint256 z = (x + 1) >> 1;
         y = x;
         while (z < y) {
             y = z;
-            z = (x / z + z) / 2;
+            z = (x / z + z) >> 1;
         }
     }
 
