d9f2ffa6e23f09ac19b9b8fd4cce37829049be3c
Emmanuel Antony <emmanuelantony2000@gmail.com>
2022-03-17 15:12:26 +0530

    Added testcases

--- test/convenience/LendGivenInsurance.spec.ts
+++ test/convenience/LendGivenInsurance.spec.ts
@@ -119,6 +119,30 @@ const testCases = [
       minBond: 1050n,
     },
   },
+  {
+    newLiquidityParams: {
+      assetIn: 10000000000000000000000n,
+      debtIn: 12000000000000000000000n,
+      collateralIn: 1000000000000000000000n,
+    },
+    lendGivenInsuranceParams: {
+      assetIn: 1000000000000000000000n,
+      insuranceOut: 67000000000000000000n,
+      minBond: 1050000000000000000000n,
+    },
+  },
+  {
+    newLiquidityParams: {
+      assetIn: 10000000000000000000000n,
+      debtIn: 12000000000000000000000n,
+      collateralIn: 1000000000000000000000n,
+    },
+    lendGivenInsuranceParams: {
+      assetIn: 1000000000000000000000000000n,
+      insuranceOut: 995000000000000000000n,
+      minBond: 1050000000000000000000n,
+    },
+  },
 ]
 
 describe('Lend Given Insurance', () => {
