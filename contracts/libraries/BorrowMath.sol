63907ffe6673d4e3a2a44709f55524d052681d70
amateur-dev <me@dipeshsukhani.dev>
2022-05-02 14:14:30 +0800

    fixing formula

--- contracts/libraries/BorrowMath.sol
+++ contracts/libraries/BorrowMath.sol
@@ -132,7 +132,7 @@ library BorrowMath {
         xReserve -= xDecrease;
 
         if (percent <= 0x80000000) {
-            uint256 yMin = assetOut; //BUG: this should be xDecrease
+            uint256 yMin = xDecrease;
             yMin *= cp.y;
             yMin = yMin.divUp(xReserve);
             yMin = yMin.shiftRightUp(4);
